164e34674e5f2dd3ca56fb859f2368da57d3ea19
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-20 12:54:48 -0400

    commit tree f65c9fbd1b847af809e775ac3cdb1f5d72c8fbd8

---
+++ index.js
@@ -0,0 +1,29 @@
+// write a constructor function called BoardMember 
+// that sets the following properties: name, homeState, and training.
+function BoardMember(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+};
+
+// veto — returns No, I must disagree
+BoardMember.prototype.veto = function () {
+    return `No, I must disagree`;
+};
+
+// approve — returns You can do that!
+BoardMember.prototype.approve = () => `You can do that!`;
+
+// doCharity — returns I like to help people.
+BoardMember.prototype.doCharity = () => `I like to help people.`;
+
+// releasePressStatement — returns You will see great things from Scuber.
+BoardMember.prototype.releasePressStatement = () => `You will see great things from Scuber.`;
+
+// sayHi — returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
+BoardMember.prototype.sayHi = function () {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+};
+
+// let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law");
+// polishedBoardMember.sayHi();
